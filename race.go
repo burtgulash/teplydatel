5ba52286e1d6e956992b35f00fae2e924c6c371c
burtgulash <burtgulash@gmail.com>
2015-09-19 17:53:11 +0200

    revert back to sync.

--- race.go
+++ race.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
+	"sync"
 	"time"
 
 	"github.com/gorilla/websocket"
@@ -30,20 +32,19 @@ type Race struct {
 	lobby        *Lobby
 
 	players map[*connection]*PlayerProgress
+	lock    sync.Mutex
 
-	receive      chan RaceMessage
-	player_join  chan *PlayerProgress
-	player_leave chan *Player
+	receive   chan RaceMessage
+	countdown chan bool
 }
 
 func NewRace(lobby *Lobby, race_code string) *Race {
 	return &Race{
-		lobby:        lobby,
-		Race_code:    race_code,
-		players:      make(map[*connection]*PlayerProgress),
-		receive:      make(chan RaceMessage, 16),
-		player_join:  make(chan *PlayerProgress, 2),
-		player_leave: make(chan *Player, 2),
+		lobby:     lobby,
+		Race_code: race_code,
+		players:   make(map[*connection]*PlayerProgress),
+		receive:   make(chan RaceMessage, 16),
+		countdown: make(chan bool, 1),
 	}
 }
 
@@ -55,17 +56,20 @@ func (r *Race) set_status(status string) {
 }
 
 func (r *Race) run() {
-	countdown := make(chan bool, 1)
-
 	r.set_status("created")
 	for {
 		select {
 
-		case <-countdown:
+		case <-r.countdown:
 			log.Printf("INFO race %s started!", r.Race_code)
+
+			r.lock.Lock()
 			r.set_status("live")
+			r.lock.Unlock()
 
 		case msg := <-r.receive:
+			r.lock.Lock()
+
 			if msg.data[0] == 'p' {
 				sender := r.players[msg.conn].player
 
@@ -75,34 +79,8 @@ func (r *Race) run() {
 				r.broadcast(b)
 			}
 
-		case pp := <-r.player_join:
-			r.players[pp.conn] = pp
-			log.Printf("INFO player %s joined race %s", pp.player.name, r.Race_code)
+			r.lock.Unlock()
 
-			if len(r.players) <= 1 {
-			} else if r.start_time == nil {
-				countdown_period := time.Second * 10
-				start_time := time.Now().Add(countdown_period)
-				r.start_time = &start_time
-
-				log.Printf("INFO race %s set start time to %s", r.Race_code, r.start_time.Format("15:04:05.000"))
-				go func() {
-					<-time.After(start_time.Sub(time.Now()))
-					countdown <- true
-				}()
-			} else {
-				still_time_to_join := time.Second * 4
-				time.Now().Before(r.start_time.Add(-still_time_to_join))
-			}
-
-		case player := <-r.player_leave:
-			for _, pp := range r.players {
-				if pp.player == player {
-					pp.conn.close()
-					log.Printf("INFO player %s left race %s", player.name, r.Race_code)
-					break
-				}
-			}
 		}
 	}
 }
@@ -114,19 +92,56 @@ func (r *Race) broadcast(message string) {
 }
 
 func (r *Race) join(player *Player, ws *websocket.Conn) (*connection, error) {
+	r.lock.Lock()
+	defer r.lock.Unlock()
+
 	conn := NewConnection(ws, player, r.receive)
-	pp := PlayerProgress{
+	pp := &PlayerProgress{
 		conn:   conn,
 		player: player,
 		race:   r,
 		done:   0,
 	}
 
-	r.player_join <- &pp
+	if len(r.players) == 0 {
+	} else if r.start_time == nil {
+		countdown_period := time.Second * 10
+		start_time := time.Now().Add(countdown_period)
+		r.start_time = &start_time
+
+		log.Printf("INFO race %s set start time to %s", r.Race_code, r.start_time.Format("15:04:05.000"))
+		go func() {
+			<-time.After(start_time.Sub(time.Now()))
+			r.countdown <- true
+		}()
+	} else {
+		// do not allow any more player joins if there is
+		// not enough time
+		still_time_to_join := time.Second * 4
+		if !time.Now().Before(
+			r.start_time.Add(-still_time_to_join)) {
+			return nil, errors.New("tried to join too late")
+		}
+	}
+
+	r.players[pp.conn] = pp
+	log.Printf("INFO player %s joined race %s", pp.player.name, r.Race_code)
+
 	return conn, nil
 }
 
 func (r *Race) leave(player *Player) error {
-	r.player_leave <- player
-	return nil
+	r.lock.Lock()
+	defer r.lock.Unlock()
+
+	for _, pp := range r.players {
+		if pp.player == player {
+			pp.conn.close()
+			log.Printf("INFO player %s left race %s", player.name, r.Race_code)
+
+			return nil
+		}
+	}
+
+	return errors.New("player does not exist")
 }
