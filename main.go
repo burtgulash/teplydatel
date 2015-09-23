a7e09586b56badfa8288bb2b7f74137bbfb65e2d
burtgulash <burtgulash@gmail.com>
2015-09-23 23:23:00 +0200

    introduce config.

--- main.go
+++ main.go
@@ -9,16 +9,42 @@ import (
 	"time"
 
 	"github.com/gorilla/pat"
+	gcfg "gopkg.in/gcfg.v1"
 )
 
 var addr = flag.String("addr", ":1338", "http server address")
-var templates = template.Must(template.ParseGlob("templates/*.html"))
+var config = flag.String("config", "config.ini", "config file")
+var templates *template.Template
+
+type Config struct {
+	Texts struct {
+		File string
+	}
+	Server struct {
+		Address string
+	}
+	Website struct {
+		Templates string
+	}
+}
 
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
 	flag.Parse()
 
-	lobby := NewLobby("res/texts.txt")
+	var cfg Config
+	err := gcfg.ReadFileInto(&cfg, *config)
+	if err != nil {
+		log.Fatalf("can't load config %s: %s", err)
+	}
+
+	addr := cfg.Server.Address
+	templates, err = template.ParseGlob(cfg.Website.Templates + "/*")
+	if err != nil {
+		log.Fatalf("can't load templates: %s", err)
+	}
+
+	lobby := NewLobby(cfg.Texts.File)
 
 	r := pat.New()
 	r.Get("/ws/{race_code}", lobby.ws_handler)
@@ -31,8 +57,8 @@ func main() {
 	http.Handle("/js/", http.StripPrefix("/js/", js))
 	http.Handle("/", r)
 
-	log.Println("INFO serving on", *addr)
-	if err := http.ListenAndServe(*addr, nil); err != nil {
+	log.Println("INFO serving on", addr)
+	if err := http.ListenAndServe(addr, nil); err != nil {
 		log.Fatal("ERROR ListenAndServe: ", err)
 	}
 }
