a66bbafc4cfea5468653a783cc8df3753aad40b7
burtgulash <burtgulash@gmail.com>
2015-09-30 15:50:23 +0200

    fix js.

--- frontend/javascript/plot.js
+++ frontend/javascript/plot.js
@@ -8,18 +8,18 @@ function Plot(container, width, height) {
 
     this.progress = {};
 
-    this.width = width - margin.left - margin.right;
-    this.height = height - margin.top - margin.bottom;
+    this.width = width - this.margin.left - this.margin.right;
+    this.height = height - this.margin.top - this.margin.bottom;
 
     this.svg = d3.select(container).append("svg")
                  .attr("width", width)
                  .attr("height", height);
 
     this.x = d3.scale.linear()
-               .range([0, this.width])
+               .range([0, this.width]);
 
     this.y = d3.scale.linear()
-               .range([this.height, 0])
+               .range([this.height, 0]);
 }
 
 function Tick(done, wpm) {
@@ -27,23 +27,24 @@ function Tick(done, wpm) {
     this.wpm = wpm;
 }
 
-Plot.prototype.add_player(player_id) {
+Plot.prototype.add_player = function(player_id) {
     this.progress[player_id] = [Tick(0, 0)];
-}
+};
 
-Plot.prototype.update_progress(player_id, done, wpm) {
+Plot.prototype.update_progress = function(player_id, done, wpm) {
     var progress = this.progress[player_id];
     if (!progress) {
-        console.log("player does not exist", {player_id: player_id});
-        return
+        console.log("player does not exist", 
+                    {player_id: player_id});
+        return;
     }
 
     var last_tick = progress[progress.length - 1];
     var current_tick = Tick(done, wpm);
     progress.push(current_tick);
     // TODO this.lineto(last_tick, current_tick)
-}
+};
 
-Plot.prototype.redraw() {
+Plot.prototype.redraw = function() {
 // update_progress for all players? and progresses recorded fo each player
-}
+};
